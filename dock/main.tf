d46c72d6122486a8b2f59125604183756a2ec3c1
Scott Lewis <scott@roof12.com>
2024-06-25 07:06:20 -0600

    cleanup

--- dock/main.tf
+++ dock/main.tf
@@ -1,6 +1,10 @@
+"""
+Dock prepared ligands with Vina.
+"""
+
 def helpMessage() {
   log.info """
-  vina pipeline
+  Dock prepared ligands with Vina. 
   """
 }
 
@@ -9,7 +13,7 @@ def validateParameters() {
     log.error 'Missing input: receptor'
     exit 1
   }
-  
+ 
   if (!params.ligands) {
     log.error 'Missing input: ligands'
     exit 1
@@ -44,36 +48,37 @@ def validateParameters() {
     log.error "Parameter must be a number: size_z"
     exit 1
   }
-
 }
 
+// Dock with Vina, saving the docked file and stdout
 process vina {
-    input:
-    path ligands
+  input:
+  path ligands
 
-    output:
-    path "*-out.txt", emit: outputs
-    path "*-docked.pdbqt", emit: dockings
+  output:
+  path "*-out.txt", emit: outputs
+  path "*-docked.pdbqt", emit: dockings
 
-    publishDir "results"
+  publishDir "results"
 
-    // Command to run AutoDock Vina
-    script:
-    """
-    vina --receptor ${params.receptor} \
-         --ligand ${ligands} \
-         --out ${ligands.baseName}-docked.pdbqt \
-         --num_modes ${params.num_modes} \
-         --exhaustiveness ${params.exhaustiveness} \
-         --center_x ${params.center_x} \
-         --center_y ${params.center_y} \
-         --center_z ${params.center_z} \
-         --size_x ${params.size_x} \
-         --size_y ${params.size_y} \
-         --size_z ${params.size_z} > ${ligands.baseName}-out.txt
-    """
+
+  script:
+  """
+  vina --receptor ${params.receptor} \
+       --ligand ${ligands} \
+       --out ${ligands.baseName}-docked.pdbqt \
+       --num_modes ${params.num_modes} \
+       --exhaustiveness ${params.exhaustiveness} \
+       --center_x ${params.center_x} \
+       --center_y ${params.center_y} \
+       --center_z ${params.center_z} \
+       --size_x ${params.size_x} \
+       --size_y ${params.size_y} \
+       --size_z ${params.size_z} > ${ligands.baseName}-out.txt
+  """
 }
 
+// Extract the ligand name and score from the docking output
 process extractScores {
   input:
   path outputs
@@ -86,14 +91,13 @@ process extractScores {
 #!/usr/bin/env python3
 
 def getLigandAndValues(filename):
-    ligand = None
-    with open(filename, "r") as file:
-        for line in file:
-            if line.startswith("Ligand: "):
-                ligand = line.strip().split()[1]
-            elif line.startswith("   1 "):
-                return [ligand] + line.split()
-
+  ligand = None
+  with open(filename, "r") as file:
+    for line in file:
+      if line.startswith("Ligand: "):
+        ligand = line.strip().split()[1]
+      elif line.startswith("   1 "):
+        return [ligand] + line.split()
 
 values = getLigandAndValues("${outputs}")
 print(",".join(values))
@@ -109,8 +113,8 @@ workflow {
   receptorName = Path.of(params.receptor).baseName
   validateParameters()
 
-  ligand_ch = channel.fromPath(params.ligands)
-  vina(ligand_ch)
+  ligandCh = channel.fromPath(params.ligands)
+  vina(ligandCh)
   extractScores(vina.out.outputs) | collectFile(name: "${receptorName}-scores.csv", storeDir: "results")
 }
 
