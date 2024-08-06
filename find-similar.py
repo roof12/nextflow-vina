b9b5477d90834f1be14309a2bdce9880c0c0ac11
Scott Lewis <scott@roof12.com>
2024-08-06 13:25:13 -0600

    write similar

--- find-similar.py
+++ find-similar.py
@@ -5,28 +5,28 @@ from indigo import Indigo
 
 indigo = Indigo()
 
-def main(molecule, ligands_file, top_count, verbose=False):
+def main(molecule, ligands_file, top_count, log=None):
     top_similarities = []  # List to store top similarities
 
     # load molecule
     molecule = indigo.loadMoleculeFromFile(molecule)
     molecules = read_indigo_molecules_from_files(indigo, filenames_from_file(ligands_file))
 
-    if verbose: print(f"Reading {ligands_file}:")
+    if log: log.write(f"Reading {ligands_file}:\n")
 
     i = 0
     for i, (mol, filename) in enumerate(molecules):
-        if verbose and i % 10000 == 0: print(".", end="", flush=True)
+        if log and i % 1 == 0: log.write(".")
         fingerprint = fingerprint_molecule(mol)
         if fingerprint is not None:
             similarity = indigo.similarity(molecule, mol, "tanimoto")
             top_similarities = update_top_similarities(top_similarities, similarity, mol, filename, top_count)
 
-    if verbose:
-        print(f"\nFound {i} molecules.")
-        print("Top similarities:")
+    if log:
+        log.write(f"\nFound {i} molecules.\n")
+        log.write("Top similarities:\n")
         for score, _, filename in top_similarities:
-            print(f"Molecule '{filename}' similarity = {score}")
+            log.write(f"Molecule '{filename}' similarity = {score}\n")
 
     for _, _, filename in top_similarities:
         print(filename)
@@ -36,11 +36,17 @@ if __name__ == "__main__":
    
     # parse args
     parser = argparse.ArgumentParser(description="Find similar molecules.")
-    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose output')
+    parser.add_argument('-l', '--logfile', help='Output file with log information')
     parser.add_argument("molecule", help="comparison molecule")
     parser.add_argument("ligands_file", help="file containing the filenames of input molecules")
     parser.add_argument("top_count", type=int, help="number of similar molecules to find")
     args = parser.parse_args()
 
-    main(args.molecule, args.ligands_file, args.top_count, args.verbose)
+    log = None
+    if args.logfile:
+        log = open(args.logfile, 'w')
+
+    main(args.molecule, args.ligands_file, args.top_count, log)
+
+    if log: log.close()
 
